c519a675653c8a3cda9065b20ae7b20d50c05070
Emre Sagir <e.sagir@student.avans.nl>
2025-02-21 11:46:42 +0100

    Finish

---
+++ MBDI_WS3_BingeQueuer/View/Components/SeriesDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SeriesDetail.swift
+//  MBDI_WS3_BingeQueuer
+//
+//  Created by Emre Sağır on 21/02/2025.
+//
+
+import SwiftUI
+
+struct SeriesDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeriesDetail()
+}
---
+++ MBDI_WS3_BingeQueuer/View/Components/SeriesList.swift
@@ -0,0 +1,8 @@
+//
+//  SeriesList.swift
+//  MBDI_WS3_BingeQueuer
+//
+//  Created by Emre Sağır on 21/02/2025.
+//
+
+import Foundation
